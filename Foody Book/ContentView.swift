1785909cda3f99b7dfeb1ae15a596f8c934dc1a0
quhaaST <asndjabbd@gmail.com>
2023-11-17 15:40:51 +0300

    [feature/project_setup] Added base for the project

---
+++ Foody Book/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Foody Book
+//
+//  Created by Aleksandr Evdokimov on 17.11.2023.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Foody Book/Foody_BookApp.swift
@@ -0,0 +1,17 @@
+//
+//  Foody_BookApp.swift
+//  Foody Book
+//
+//  Created by Aleksandr Evdokimov on 17.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodyBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
