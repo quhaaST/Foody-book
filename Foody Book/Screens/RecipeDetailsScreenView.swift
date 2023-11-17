ac7ce1bc7e20d1c0a87fd392e737212dcff66aa4
quhaaST <asndjabbd@gmail.com>
2023-11-17 16:04:38 +0300

    [feature/bottom_navigation_bar_support] Base for screens and bottom navigation bar

---
+++ Foody Book/Screens/RecipeDetailsScreenView.swift
@@ -0,0 +1,21 @@
+//
+//  RecipeDetailsScreenView.swift
+//  Foody Book
+//
+//  Created by Aleksandr Evdokimov on 17.11.2023.
+//
+
+import SwiftUI
+
+struct RecipeDetailsScreenView: View {
+    var body: some View {
+        Text("Recipe details screen view!")
+            .padding()
+    }
+}
+
+struct RecipeDetailsScreenView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeDetailsScreenView()
+    }
+}
